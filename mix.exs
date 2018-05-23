ea302b697b4571181b4402ac7ceed6b8e79e79cb
Nick Veys <nick@codelever.com>
2018-05-23 16:11:56 -0500

    Update deps, add ecto

--- mix.exs
+++ mix.exs
@@ -22,8 +22,9 @@ defmodule Churrobot.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:inflex, "~> 1.10.0"},
-      {:slack, "~> 0.12.0"}
+      {:ecto, "~> 2.2"},
+      {:inflex, "~> 1.10"},
+      {:slack, "~> 0.12"}
     ]
   end
 end
